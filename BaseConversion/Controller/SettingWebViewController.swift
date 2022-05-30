d9ebba6472ff6e6cdde850d692d81e51cbb29fe1
Kota <maedakoutaa@gmail.com>
2022-05-31 08:11:17 +0900

    SettingWebViewControllerの追加

---
+++ BaseConversion/Controller/SettingWebViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingWebViewController.swift
+//  BaseConversion
+//
+//  Created by 前田航汰 on 2022/05/31.
+//
+
+import UIKit
+
+class SettingWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
