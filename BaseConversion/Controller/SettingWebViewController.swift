c09ece0ce6fd148921c65ef8c241bd190e9b7297
Kota <maedakoutaa@gmail.com>
2022-05-31 08:16:54 +0900

    webview画面の土台完成

--- BaseConversion/Controller/SettingWebViewController.swift
+++ BaseConversion/Controller/SettingWebViewController.swift
@@ -6,11 +6,60 @@
 //
 
 import UIKit
+import WebKit
 
-class SettingWebViewController: UIViewController {
+class SettingWebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate  {
+
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var backButton: UIBarButtonItem!
+    @IBOutlet private weak var forwardButton: UIBarButtonItem!
+    private var presentUrl = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        judgeToolBarButton()
+    }
+
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        webView.goBack()
+        judgeToolBarButton()
+    }
+
+    @IBAction private func didTapForwardButton(_ sender: Any) {
+        webView.goForward()
+        judgeToolBarButton()
+    }
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("読み込み開始")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("読み込み完了")
+    }
+
+    private func judgeToolBarButton() {
+        if webView.canGoBack {
+            backButton.isEnabled = true
+        } else {
+            backButton.isEnabled = false
+        }
+
+        if webView.canGoForward {
+            forwardButton.isEnabled = true
+        } else {
+            forwardButton.isEnabled = false
+        }
+    }
+
+    @IBAction func didTapExitButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func catchUrl(url: String) {
+        presentUrl = url
     }
 
 }
